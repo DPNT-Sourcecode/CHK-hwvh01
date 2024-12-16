87e60ac3668b8472383304fba8e3d893325b0199
sbx_user1051 <sbx_user1051@169.254.8.58>
2024-12-16 22:10:50 +0000

    Mon Dec 16 22:10:50 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -34,51 +34,54 @@ def checkout(skus):
         "F": 10,
     }
 
-    for i in range(0, len(skus)):
-        if isinstance(skus[i], str) and skus[i].isalpha() and skus[i].capitalize():
-            if skus[i] in data:
-                match skus[i]:
-                    case 'A':
-                        a += 1
-                        aPrice += data[skus[i]]
-                        if a == 3:
-                            aPrice -= 20
-                        if a == 5:
-                            aPrice -= 30
-                            a = 0
-                    case 'B':
-                        b += 1
-                        bPrice += data[skus[i]]
-                        if b == 2:
-                            bPrice -= 15
-                            b = 0
-                    case 'C':
-                        c += 1
-                        cPrice += data[skus[i]]
-                    case 'D':
-                        d += 1
-                        dPrice += data[skus[i]]
-                    case 'E':
-                        e += 1
-                        ePrice += data[skus[i]]
-                        if e == 1:
-                            ePrice += data[skus[i]]
-                            freeB += data["B"]
-                            e = 0
-                    case 'F':
-                        f += 1
-                        fPrice += data[skus[i]]
-                        if f == 2:  # This applies the new offer for F (3 Fs = 1 free)
-                            freeF += data["F"]
-                            f = 0
+    for sku in skus:
+        if isinstance(sku, str) and sku.isalpha() and sku.capitalize():
+            if sku in data:
+                # Apply the offers based on the item
+                if sku == 'A':
+                    a += 1
+                    aPrice += data['A']
+                    if a == 3:
+                        aPrice -= 20  # Discount for 3 A's
+                    if a == 5:
+                        aPrice -= 30  # Discount for 5 A's
+                        a = 0
+                elif sku == 'B':
+                    b += 1
+                    bPrice += data['B']
+                    if b == 2:
+                        bPrice -= 15  # Discount for 2 B's
+                        b = 0
+                elif sku == 'C':
+                    c += 1
+                    cPrice += data['C']
+                elif sku == 'D':
+                    d += 1
+                    dPrice += data['D']
+                elif sku == 'E':
+                    e += 1
+                    ePrice += data['E']
+
+                    # When 2 E's are encountered, apply the offer
+                    if e == 2:
+                        ePrice -= 40  # Subtract the excess cost (2 * 40)
+                        freeB += 30  # Add free B (as 2 E's give 1 free B)
+                        e = 0  # Reset E count after the offer
+                elif sku == 'F':
+                    f += 1
+                    fPrice += data['F']
+                    if f == 3:  # "Buy 2 get 1 free" for F
+                        freeF += 1
+                        f = 0
             else:
-                return -1
+                return -1  # Invalid SKU
         else:
-            return -1
+            return -1  # Invalid SKU
     cost = sum([aPrice, bPrice, cPrice, dPrice, ePrice, fPrice])
     cost -= freeB
     cost -= freeF
     return cost
+
 
 
 
