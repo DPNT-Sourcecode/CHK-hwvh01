47edcb17234617a4da736b923d665e8371e45180
sbx_user1051 <sbx_user1051@169.254.8.58>
2024-12-16 20:57:20 +0000

    Mon Dec 16 20:57:20 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,11 +7,24 @@ def checkout(skus):
     aCount = int(0)
     bCount = int(0)
 
-    current = ['a', 'b', 'c', 'd']
+    cost = 0
+
+    data = {
+        "A": 50,
+        "B": 30,
+        "C": 20,
+        "D": 15
+    }
+
+    special_offers = {
+        'A': (3, 130),  # 3 A's for 130
+        'B': (2, 45)  # 2 B's for 45
+    }
 
     for sku in skus:
         if isinstance(sku, str) and sku.isalpha():
-            if sku not in current:
+            if sku not in data.keys():
                 return -1
             else:
-                if
+
+
