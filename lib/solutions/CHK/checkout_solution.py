8e2d66c507c2d0e2dc71551547282cc79b4f63bb
sbx_user1051 <sbx_user1051@169.254.8.58>
2024-12-16 22:05:52 +0000

    Mon Dec 16 22:05:52 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,6 +2,28 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+
+    basket = []
+
+    aPrice = 0
+    bPrice = 0
+    cPrice = 0
+    dPrice = 0
+    ePrice = 0
+    fPrice = 0
+
+    a = 0
+    b = 0
+    c = 0
+    d = 0
+    e = 0
+    f = 0
+
+    freeB = 0
+    freeF = 0
+
+    offset = 0
+
     # Prices for items
     data = {
         "A": 50,
@@ -12,60 +34,52 @@ def checkout(skus):
         "F": 10,
     }
 
-    # Initialize variables for counts and prices
-    aPrice = bPrice = cPrice = dPrice = ePrice = fPrice = 0
-    a = b = c = d = e = f = 0
-    freeB = freeF = 0
-
-    for sku in skus:
-        if isinstance(sku, str) and sku.isalpha() and sku.capitalize():
-            if sku in data:
-                # Apply the offers based on the item
-                if sku == 'A':
-                    a += 1
-                    aPrice += data['A']
-                    if a == 3:
-                        aPrice -= 20  # Discount for 3 A's
-                    if a == 5:
-                        aPrice -= 30  # Discount for 5 A's
-                        a = 0
-                elif sku == 'B':
-                    b += 1
-                    bPrice += data['B']
-                    if b == 2:
-                        bPrice -= 15  # Discount for 2 B's
-                        b = 0
-                elif sku == 'C':
-                    c += 1
-                    cPrice += data['C']
-                elif sku == 'D':
-                    d += 1
-                    dPrice += data['D']
-                elif sku == 'E':
-                    e += 1
-                    ePrice += data['E']
-                    # Apply special offer for 2 E's at 80
-                    if e == 2:
-                        ePrice -= 40  # Correct pricing for 2 E's
-                        freeB += 1  # Free B with 2 E's
-                        e = 0
-                elif sku == 'F':
-                    f += 1
-                    fPrice += data['F']
-                    if f == 3:  # "Buy 2 get 1 free" for F
-                        freeF += 1
-                        f = 0
+    for i in range(0, len(skus)):
+        if isinstance(skus[i], str) and skus[i].isalpha() and skus[i].capitalize():
+            if skus[i] in data:
+                match skus[i]:
+                    case 'A':
+                        a += 1
+                        aPrice += data[skus[i]]
+                        if a == 3:
+                            aPrice -= 20
+                        if a == 5:
+                            aPrice -= 30
+                            a = 0
+                    case 'B':
+                        b += 1
+                        bPrice += data[skus[i]]
+                        if b == 2:
+                            bPrice -= 15
+                            b = 0
+                    case 'C':
+                        c += 1
+                        cPrice += data[skus[i]]
+                    case 'D':
+                        d += 1
+                        dPrice += data[skus[i]]
+                    case 'E':
+                        e += 1
+                        ePrice += data[skus[i]]
+                        # if e == 1:
+                        #     ePrice += data[skus[i]]
+                        #     freeB += data["B"]
+                        #     e = 0
+                    case 'F':
+                        f += 1
+                        fPrice += data[skus[i]]
+                        if f == 2:  # This applies the new offer for F (3 Fs = 1 free)
+                            freeF += data["F"]
+                            f = 0
             else:
-                return -1  # Invalid SKU
+                return -1
         else:
-            return -1  # Invalid SKU
+            return -1
+    cost = sum([aPrice, bPrice, cPrice, dPrice, ePrice, fPrice])
+    cost -= freeB
+    cost -= freeF
+    return cost
 
-    # Total cost calculation
-    total_cost = aPrice + bPrice + cPrice + dPrice + ePrice + fPrice
-    total_cost -= freeB * data['B']  # Subtract the cost of free B's
-    total_cost -= freeF * data['F']  # Subtract the cost of free F's
-
-    return total_cost
 
 
 
