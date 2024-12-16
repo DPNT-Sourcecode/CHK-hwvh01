0ee7a63a9440dc1e0baac57c568fe9ec55bae12b
sbx_user1051 <sbx_user1051@169.254.8.58>
2024-12-16 20:51:20 +0000

    Mon Dec 16 20:51:20 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,18 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+
+    aCount = int(0)
+    bCount = int(0)
+
+    if isinstance(skus, str):
+        for sku in skus:
+            if != sku.isalnum():
+                if sku = 'a':
+                    aCount += 1
+                elif sku = 'b'
+
+
+    else:
+        return -1
+
