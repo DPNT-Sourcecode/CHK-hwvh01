4941e3daf99d0b0d9cc6ca1c48c67f43837f4dc1
sbx_user1051 <sbx_user1051@169.254.8.58>
2024-12-16 21:00:17 +0000

    Mon Dec 16 21:00:17 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -23,8 +23,34 @@ def checkout(skus):
 
     for sku in skus:
         if isinstance(sku, str) and sku.isalpha():
-            if sku not in data.keys():
+            if sku not in data:
                 return -1
-            else:
+
+            if sku == 'A':
+                aCount += 1
+            elif sku == 'B':
+                bCount += 1
+            elif sku == 'C':
+                cost += data[sku]
+            elif sku == 'D':
+                cost += data[sku]
+        else:
+            return -1
+
+    if aCount >= 3:
+        offer_count = aCount // 3
+        cost += offer_count * special_offers['A'][1]
+        aCount %= 3
+
+    if bCount >= 2:
+        offer_count = bCount // 2
+        cost += offer_count * special_offers['B'][1]
+        bCount %= 2
+
+    cost += aCount * data['A']
+    cost += bCount * data['B']
+
+    return cost
+
 
 
